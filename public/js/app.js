a8ef7165c600825b4b0270961b161e3684ebd7d9
pkeogh1024@yahoo.com <Tiesha Sims>
2016-12-19 15:53:11 -0700

    modified index and js

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+//create out "MyApp" angular application with the
+// 'ui router' as a dependency.
+var app = angular.module ('MyApp', ['ui.router']);
+
+//Configure the 'MyApp' angular application.
+app.config([
+    //Bring in the dependency objects by name.
+    '$stateProvider',
+
+    //Provide a callback function to use one all
+    //the dependencies have been loaded.
+    function ($stateProvider) {
+        //Setup and define out page states.
+        $stateProvider
+            .state ('home',{
+                url: '/',
+                template:'<h2>Home Page</h2>'
+        })
+            .state ('about', {
+                url: '/about',
+                template: '<h2>About Page</h2>'
+        })
+            .state ('contact',{
+                url: '/contact',
+                template: '<h2>Contact Page</h2>'
+        })
+        ;
+    }
+])
